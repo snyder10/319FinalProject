73f411f8a758bfa9ce9faa1434b749e1f4b0160c
unknown <arsnyder1011@gmail.com>
2024-05-07 17:10:53 -0500

    Switched order on employee view

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -194,17 +194,17 @@ const App = () => {
                 </del>
               </div>
               <p className="text-lg font-semibold text-black cursor-auto my-3">Inventory: {product.inventory}</p>
+              <div className="flex justify-between items-center w-full">
+                <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={() => removeFromCart(product.name)}>-</button>
+                <span className="border-solid select-none text-lg font-bold text-black">{cart[product.name] ? cart[product.name] : 0}</span>
+                <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={() => addToCart(product.name)} >+</button>
+              </div>
               {user.employee &&
                 <form className="max-w-sm mx-auto" id={product._id} onSubmit={submitUpdate}>
                   <label htmlFor={`number-input-${product._id}`} className="block mb-2 text-sm font-medium text-gray-900">Update Inventory:</label>
                   <input type="number" id={`number-input-${product._id}`} name="inventory" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={product.inventory} />
                 </form>
               }
-              <div className="flex justify-between items-center w-full">
-                <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={() => removeFromCart(product.name)}>-</button>
-                <span className="border-solid select-none text-lg font-bold text-black">{cart[product.name] ? cart[product.name] : 0}</span>
-                <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={() => addToCart(product.name)} >+</button>
-              </div>
             </div>
           </div>
         ))}
